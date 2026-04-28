1d5affe82aeb2f374d3c9c0800eac569d90710ab
James Holloway <james.holloway94@gmail.com>
2026-04-28 13:28:05 +0000

    tag postgres checkpoint sync for replication trail

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-28 13:07:28 UTC
+# Last sync: 2026-04-28 13:28:05 UTC
