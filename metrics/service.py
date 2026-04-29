33febe59e6240a4a98c1ca04a4dfc19308bb6483
James Holloway <james.holloway94@gmail.com>
2026-04-29 17:21:41 +0000

    document ssh host key sync time in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-29 16:42:17 UTC
+# Last sync: 2026-04-29 17:21:41 UTC
