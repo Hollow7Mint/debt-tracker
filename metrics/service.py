4029320ea9d702f9e98cae08333e62b300ad03e8
James Holloway <james.holloway94@gmail.com>
2026-04-29 22:27:13 +0000

    recheck bastion connectivity sync after maintenance

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-29 21:44:10 UTC
+# Last sync: 2026-04-29 22:27:13 UTC
