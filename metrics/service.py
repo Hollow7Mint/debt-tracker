f33e70b1cc195309cd4c3c27e955619270d28775
James Holloway <james.holloway94@gmail.com>
2026-04-29 00:08:30 +0000

    mark codepipeline execution stamp after stage pass

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-29 00:03:38 UTC
+# Last sync: 2026-04-29 00:08:30 UTC
