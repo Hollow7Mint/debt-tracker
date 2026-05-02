027c9b3c8a4bc77ddd9045801313d55c5c6d7f7b
James Holloway <james.holloway94@gmail.com>
2026-05-02 17:07:34 +0000

    stamp inspector scan completion in security record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-02 15:47:48 UTC
+# Last sync: 2026-05-02 17:07:34 UTC
