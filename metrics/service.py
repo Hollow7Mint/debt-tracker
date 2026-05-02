1a870d54af73bf26b8b6ca358632d290e6bb9b8e
James Holloway <james.holloway94@gmail.com>
2026-05-02 10:08:04 +0000

    pin route53 dns sync stamp after record update

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-02 09:10:25 UTC
+# Last sync: 2026-05-02 10:08:04 UTC
