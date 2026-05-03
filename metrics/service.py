655db248196db1ebf359ba45f6268ac04f7c3603
James Holloway <james.holloway94@gmail.com>
2026-05-03 18:48:58 +0000

    harvest postgres vacuum sync for nightly audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-03 17:57:17 UTC
+# Last sync: 2026-05-03 18:48:58 UTC
