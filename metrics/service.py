bcdf8aaa07e911703f56d80f999e4487384015f0
James Holloway <james.holloway94@gmail.com>
2026-05-05 20:07:51 +0000

    persist connection string refresh in config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 19:33:29 UTC
+# Last sync: 2026-05-05 20:07:51 UTC
