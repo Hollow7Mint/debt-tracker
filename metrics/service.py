9c5ee6b5119a84a3ffcbb039a785436b670cd21a
James Holloway <james.holloway94@gmail.com>
2026-05-05 00:59:11 +0000

    align vpc subnet allocation time in config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-04 23:36:10 UTC
+# Last sync: 2026-05-05 00:59:11 UTC
