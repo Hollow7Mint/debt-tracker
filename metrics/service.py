5c86b48070e5449c48f1819c91e9852e45651b44
James Holloway <james.holloway94@gmail.com>
2026-05-05 08:43:31 +0000

    audit db backup completion time in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 07:19:59 UTC
+# Last sync: 2026-05-05 08:43:31 UTC
