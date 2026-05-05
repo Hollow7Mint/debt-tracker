2674d5fa7694882e0425eac148592d86807fb94e
James Holloway <james.holloway94@gmail.com>
2026-05-05 04:42:26 +0000

    restamp iam permission boundary sync after review

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 03:09:28 UTC
+# Last sync: 2026-05-05 04:42:26 UTC
