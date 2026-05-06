ba243950d4fb42aca5fedb1815fe2ebb92e102bc
James Holloway <james.holloway94@gmail.com>
2026-05-06 00:42:39 +0000

    carry waf ip set sync stamp to config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 23:36:54 UTC
+# Last sync: 2026-05-06 00:42:39 UTC
