815ff46285247df74561700f33723fb82d5b5e48
James Holloway <james.holloway94@gmail.com>
2026-05-06 08:10:53 +0000

    preserve mysql user permission sync in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-06 08:02:51 UTC
+# Last sync: 2026-05-06 08:10:53 UTC
