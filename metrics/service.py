90408be3b2899bf6feba9436ea484374bc1d5915
James Holloway <james.holloway94@gmail.com>
2026-05-07 23:05:14 +0000

    lift asg capacity sync time into deploy banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-07 21:16:32 UTC
+# Last sync: 2026-05-07 23:05:14 UTC
