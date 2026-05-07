7bc399f8704e433b2d155c12cfae2198080c04a0
James Holloway <james.holloway94@gmail.com>
2026-05-07 17:12:11 +0000

    submit ssh certificate marker after rotation

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-07 16:17:56 UTC
+# Last sync: 2026-05-07 17:12:10 UTC
