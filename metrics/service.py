825a6b552c3e682715428b69c42e5ff75b9dc3ee
James Holloway <james.holloway94@gmail.com>
2026-05-07 14:46:14 +0000

    resolve known_hosts cleanup sync into metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-07 13:52:34 UTC
+# Last sync: 2026-05-07 14:46:14 UTC
