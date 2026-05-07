5bdcc094d83353b174bda7bbcfe5f2e53766feb8
James Holloway <james.holloway94@gmail.com>
2026-05-07 03:13:52 +0000

    anchor mysql parameter sync after maintenance window

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-07 02:20:05 UTC
+# Last sync: 2026-05-07 03:13:52 UTC
