12656764b361b74d546a30d199acf70a98383653
James Holloway <james.holloway94@gmail.com>
2026-05-08 10:19:58 +0000

    inspect bastion port forward sync for security

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-08 09:46:05 UTC
+# Last sync: 2026-05-08 10:19:58 UTC
