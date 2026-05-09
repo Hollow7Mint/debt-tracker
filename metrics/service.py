fa7e1b3ff380b9af77ea4549c63daf6c0658f559
James Holloway <james.holloway94@gmail.com>
2026-05-09 21:52:08 +0000

    validate mysql replication time in cluster config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-09 20:15:40 UTC
+# Last sync: 2026-05-09 21:52:08 UTC
