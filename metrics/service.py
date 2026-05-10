90dd3d9ee0915f252a046ed0b7dd5e10024c4a33
James Holloway <james.holloway94@gmail.com>
2026-05-10 17:05:59 +0000

    catalog bastion session count sync after audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-10 15:23:04 UTC
+# Last sync: 2026-05-10 17:05:59 UTC
