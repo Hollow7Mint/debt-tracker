75c5473dcdc0fd51d0946221db5c9b630cb33dfa
James Holloway <james.holloway94@gmail.com>
2026-05-10 05:57:13 +0000

    store redis eviction policy sync for cache audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-10 04:39:52 UTC
+# Last sync: 2026-05-10 05:57:13 UTC
