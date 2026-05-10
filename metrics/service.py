b62e2c877f208e84e51d3f5be9a00c34cbf549f8
James Holloway <james.holloway94@gmail.com>
2026-05-10 17:39:29 +0000

    note postgres extension sync in config record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-10 17:05:59 UTC
+# Last sync: 2026-05-10 17:39:29 UTC
