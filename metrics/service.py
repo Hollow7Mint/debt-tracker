f70596bf5562f873f42833f9ec33db47ca1b3f17
James Holloway <james.holloway94@gmail.com>
2026-05-11 08:12:23 +0000

    resolve mysql backup sync into metadata file

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 07:36:20 UTC
+# Last sync: 2026-05-11 08:12:23 UTC
