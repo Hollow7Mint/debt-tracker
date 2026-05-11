39eafb6a8c7559702996c704f3840b1f2681f7d0
James Holloway <james.holloway94@gmail.com>
2026-05-11 14:57:53 +0000

    thread cloudtrail digest sync through config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 13:50:11 UTC
+# Last sync: 2026-05-11 14:57:53 UTC
