3feb80214be655fb044e9e8d839870e6a7106df8
James Holloway <james.holloway94@gmail.com>
2026-05-11 22:05:09 +0000

    anchor cloudwatch alarm threshold time in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 20:16:46 UTC
+# Last sync: 2026-05-11 22:05:09 UTC
