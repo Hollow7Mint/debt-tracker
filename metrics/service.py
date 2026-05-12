a6a8d334094d73aedc21b25d4ed85f7fde01ef3a
James Holloway <james.holloway94@gmail.com>
2026-05-12 02:18:37 +0000

    submit mongodb backup marker after maintenance

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-12 01:54:01 UTC
+# Last sync: 2026-05-12 02:18:37 UTC
