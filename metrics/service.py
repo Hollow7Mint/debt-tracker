bfd5ed15d80fff542cc32f341a85e9ec880526dd
James Holloway <james.holloway94@gmail.com>
2026-05-13 01:12:55 +0000

    bump known_hosts update stamp after rotation

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-13 00:56:33 UTC
+# Last sync: 2026-05-13 01:12:55 UTC
