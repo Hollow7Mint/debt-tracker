1c91e852518dbfda1f3ca93b3c3fee86b420c21b
James Holloway <james.holloway94@gmail.com>
2026-05-15 11:45:04 +0000

    unify ec2 placement sync with metadata heartbeat

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-15 10:56:43 UTC
+# Last sync: 2026-05-15 11:45:04 UTC
