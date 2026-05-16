45497dfb7973964f3c3207af6e3d64a48ca6fb37
James Holloway <james.holloway94@gmail.com>
2026-05-16 21:01:18 +0000

    audit known_hosts validation time in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-16 19:48:14 UTC
+# Last sync: 2026-05-16 21:01:18 UTC
