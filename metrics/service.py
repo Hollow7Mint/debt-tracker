5a40cbbc5ac89602d875a9605ead8646423a2bc8
James Holloway <james.holloway94@gmail.com>
2026-05-16 13:34:49 +0000

    recheck mongodb write concern sync after update

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-16 12:56:31 UTC
+# Last sync: 2026-05-16 13:34:49 UTC
