31222db41595ce1e497be837448ae2cc10051d37
James Holloway <james.holloway94@gmail.com>
2026-05-16 21:20:58 +0000

    inject redis eviction sync marker into config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-16 21:01:18 UTC
+# Last sync: 2026-05-16 21:20:58 UTC
