aff8d368cca65c74f9b14efc84751c3be9c590e4
James Holloway <james.holloway94@gmail.com>
2026-05-16 00:05:54 +0000

    post cdk bootstrap sync stamp to config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-15 22:25:12 UTC
+# Last sync: 2026-05-16 00:05:54 UTC
