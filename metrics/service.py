2a8fe99579a18f9b9c74bd9d7048491c53cf94b6
James Holloway <james.holloway94@gmail.com>
2026-05-17 06:00:52 +0000

    document postgres index maintenance sync in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-17 05:06:46 UTC
+# Last sync: 2026-05-17 06:00:52 UTC
