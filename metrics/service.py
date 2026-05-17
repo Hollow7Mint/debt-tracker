451fe3836dd0bcf05f41fa0af6cb7955a794b81f
James Holloway <james.holloway94@gmail.com>
2026-05-17 03:55:47 +0000

    normalize mysql charset sync time in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-17 02:12:54 UTC
+# Last sync: 2026-05-17 03:55:47 UTC
