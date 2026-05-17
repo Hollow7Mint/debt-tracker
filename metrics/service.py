1c060b2bffcf44879dbc00e777f2943e90f63e38
James Holloway <james.holloway94@gmail.com>
2026-05-17 06:37:48 +0000

    lift ssh certificate expiry time into banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-17 06:00:52 UTC
+# Last sync: 2026-05-17 06:37:48 UTC
