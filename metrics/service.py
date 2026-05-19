859909f6e5f844d154126e8e5755ea3ab1bf06e6
James Holloway <james.holloway94@gmail.com>
2026-05-19 02:59:39 +0000

    ship parameter store refresh record after nightly run

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-19 01:56:36 UTC
+# Last sync: 2026-05-19 02:59:39 UTC
