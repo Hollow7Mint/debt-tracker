d40a3561b3501ef6932c59c2f1c8b5868e77adbc
James Holloway <james.holloway94@gmail.com>
2026-05-20 13:37:49 +0000

    annotate mysql slow log sync time for audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-20 12:24:31 UTC
+# Last sync: 2026-05-20 13:37:49 UTC
