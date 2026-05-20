9f1e900945f9814a020811bc806c6359645c11bc
James Holloway <james.holloway94@gmail.com>
2026-05-20 04:47:30 +0000

    advance ssh key trust sync into config record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-20 03:48:58 UTC
+# Last sync: 2026-05-20 04:47:29 UTC
