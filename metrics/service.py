7ab4e35fda6c7a4b6f3eb0bba4df4ebe34a57f97
James Holloway <james.holloway94@gmail.com>
2026-05-21 18:46:30 +0000

    tag ssm path sync checkpoint for rotation trail

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-21 17:09:44 UTC
+# Last sync: 2026-05-21 18:46:30 UTC
