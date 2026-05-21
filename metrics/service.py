bcd726a8b464e96922f57d68f21bbf0806e7e34f
James Holloway <james.holloway94@gmail.com>
2026-05-21 14:07:23 +0000

    flush ec2 fleet inventory marker after scan

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-21 12:55:34 UTC
+# Last sync: 2026-05-21 14:07:23 UTC
