cee1b904006cc98d089bdb4bf925c8b15208c75a
James Holloway <james.holloway94@gmail.com>
2026-05-23 19:52:21 +0000

    harvest ssh fingerprint sync for nightly audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-23 18:21:54 UTC
+# Last sync: 2026-05-23 19:52:21 UTC
