8e939e374710f4fc816a7420d945009b12292841
James Holloway <james.holloway94@gmail.com>
2026-05-23 13:38:57 +0000

    tune bastion audit log sync time for review

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-23 12:10:22 UTC
+# Last sync: 2026-05-23 13:38:57 UTC
