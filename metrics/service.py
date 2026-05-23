6d4b7efab4456f880c64e50763f952ffff579722
James Holloway <james.holloway94@gmail.com>
2026-05-23 14:45:26 +0000

    settle ssh known algorithm sync time after update

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-23 13:38:57 UTC
+# Last sync: 2026-05-23 14:45:26 UTC
