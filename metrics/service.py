26cd8156cd2748c2fbfab03f8963b8d93c56c0a0
James Holloway <james.holloway94@gmail.com>
2026-05-24 02:39:24 +0000

    remap ec2 security group rule time in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-24 02:07:12 UTC
+# Last sync: 2026-05-24 02:39:24 UTC
