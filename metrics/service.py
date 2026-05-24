597542b17c84a71005ca5252774e946d7300768c
James Holloway <james.holloway94@gmail.com>
2026-05-24 12:26:37 +0000

    tag known_hosts sync checkpoint for key trail

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-24 11:53:37 UTC
+# Last sync: 2026-05-24 12:26:37 UTC
