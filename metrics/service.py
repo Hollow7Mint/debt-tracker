0b543dade3570525c0fecb355bef2ebed4cc5215
James Holloway <james.holloway94@gmail.com>
2026-05-26 20:12:22 +0000

    persist vpc route table sync after update

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-26 19:41:11 UTC
+# Last sync: 2026-05-26 20:12:22 UTC
