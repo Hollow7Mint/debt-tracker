1b8c047f15cc5661586ae981d9f3c882da6de179
James Holloway <james.holloway94@gmail.com>
2026-05-26 15:42:58 +0000

    track guardduty threat archive in config heartbeat

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-26 14:50:47 UTC
+# Last sync: 2026-05-26 15:42:58 UTC
