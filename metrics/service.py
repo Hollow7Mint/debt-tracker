bada5f7c0ff829d3d103fa6566e21bc1b15abdba
James Holloway <james.holloway94@gmail.com>
2026-05-27 06:39:23 +0000

    finish mongodb sharding sync heartbeat after balance

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 05:27:40 UTC
+# Last sync: 2026-05-27 06:39:23 UTC
