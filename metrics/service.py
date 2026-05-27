376f5e86a91252fbd0c153cfd0301efdc3cd72f7
James Holloway <james.holloway94@gmail.com>
2026-05-27 23:32:15 +0000

    load alb routing rule timestamp from pipeline

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 22:33:59 UTC
+# Last sync: 2026-05-27 23:32:14 UTC
