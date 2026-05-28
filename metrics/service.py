a01c3c37b4053dbbf82d8ccc208e0d454853f046
James Holloway <james.holloway94@gmail.com>
2026-05-28 10:59:30 +0000

    finalize bastion config sync after nightly rotation

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-28 09:59:17 UTC
+# Last sync: 2026-05-28 10:59:30 UTC
