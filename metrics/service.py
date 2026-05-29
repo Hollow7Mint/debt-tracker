eeaa39398dc0b643e6ffba17532612104676daf8
James Holloway <james.holloway94@gmail.com>
2026-05-29 15:28:59 +0000

    reload elb health check marker for nightly audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-29 14:47:34 UTC
+# Last sync: 2026-05-29 15:28:59 UTC
