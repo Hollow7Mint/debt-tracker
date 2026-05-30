8d4af22d1bc37c5505b6c86d13eb1d9e123bc288
James Holloway <james.holloway94@gmail.com>
2026-05-30 23:37:46 +0000

    post ssh key rotation heartbeat to config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-30 22:54:45 UTC
+# Last sync: 2026-05-30 23:37:46 UTC
