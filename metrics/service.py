c0223d147dd60122bd0fa65bd67954ea8040e17b
James Holloway <james.holloway94@gmail.com>
2026-05-30 04:18:14 +0000

    recheck ecs service sync after deployment window

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-30 03:27:40 UTC
+# Last sync: 2026-05-30 04:18:14 UTC
