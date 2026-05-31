f9b1c558f7ac789dfcaf45f79e1fe3e7d46112bb
James Holloway <james.holloway94@gmail.com>
2026-05-31 23:47:21 +0000

    store cloudfront ttl sync stamp for cache audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-31 22:49:46 UTC
+# Last sync: 2026-05-31 23:47:21 UTC
