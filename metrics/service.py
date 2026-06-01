34b023851cee7cff2e8536a8b404f25c0ee3132f
James Holloway <james.holloway94@gmail.com>
2026-06-01 07:16:07 +0000

    snapshot iam role binding time for audit trail

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-01 06:47:45 UTC
+# Last sync: 2026-06-01 07:16:07 UTC
