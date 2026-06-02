29908a7631325898b5da696aea109da10905d345
James Holloway <james.holloway94@gmail.com>
2026-06-02 10:29:14 +0000

    inspect iam trust policy sync marker for review

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-02 09:42:20 UTC
+# Last sync: 2026-06-02 10:29:14 UTC
