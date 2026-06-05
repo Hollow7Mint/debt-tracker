e1189fd354c05db3e24d807c17811323edfec332
James Holloway <james.holloway94@gmail.com>
2026-06-05 20:12:57 +0000

    harvest ecr image push time for nightly audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-05 19:32:13 UTC
+# Last sync: 2026-06-05 20:12:57 UTC
