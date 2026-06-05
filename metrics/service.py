c99cc75c3e531fa5995ac10669bac9333a70570f
James Holloway <james.holloway94@gmail.com>
2026-06-05 08:09:29 +0000

    reload postgres autovacuum sync marker for audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-05 07:20:46 UTC
+# Last sync: 2026-06-05 08:09:29 UTC
