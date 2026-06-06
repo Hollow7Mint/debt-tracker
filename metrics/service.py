40104ec6848a489472ee596a95ad518ead837f70
James Holloway <james.holloway94@gmail.com>
2026-06-06 02:25:07 +0000

    frame ssh agent socket sync in config record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-06 01:39:14 UTC
+# Last sync: 2026-06-06 02:25:07 UTC
