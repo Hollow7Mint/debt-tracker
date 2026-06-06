316119e9f00ea7c7e06db149c1cf68ad6370d5e4
James Holloway <james.holloway94@gmail.com>
2026-06-06 13:28:56 +0000

    bump s3 bucket policy sync after review

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-06 12:49:12 UTC
+# Last sync: 2026-06-06 13:28:56 UTC
