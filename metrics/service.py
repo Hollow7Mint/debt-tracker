6a55fc0d13e87591e222bb15b56de67ad0395810
James Holloway <james.holloway94@gmail.com>
2026-06-07 08:37:46 +0000

    load bastion session log sync from pipeline

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-07 07:20:26 UTC
+# Last sync: 2026-06-07 08:37:46 UTC
