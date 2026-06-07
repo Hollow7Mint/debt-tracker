ad2b4b37947b4160cfac2a0ed05c3fe0034517d0
James Holloway <james.holloway94@gmail.com>
2026-06-07 12:32:54 +0000

    snapshot mysql instance time for database audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-07 11:49:39 UTC
+# Last sync: 2026-06-07 12:32:54 UTC
