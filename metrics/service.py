c1229c47a37fc09b99106d2fc181290873c5cf1e
James Holloway <james.holloway94@gmail.com>
2026-06-09 13:23:45 +0000

    thread postgres wal sync through config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-09 12:33:00 UTC
+# Last sync: 2026-06-09 13:23:45 UTC
