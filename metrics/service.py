e92c7a4456fcb056fde2a1f8c493b9a88dd074ed
James Holloway <james.holloway94@gmail.com>
2026-06-09 21:54:24 +0000

    pin mongodb shard balancing time in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-09 20:47:38 UTC
+# Last sync: 2026-06-09 21:54:24 UTC
