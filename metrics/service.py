c97a4cb1886461a150c263fbe87e47b9b1034076
James Holloway <james.holloway94@gmail.com>
2026-06-10 22:38:51 +0000

    carry mongodb oplog sync stamp to config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-10 21:51:47 UTC
+# Last sync: 2026-06-10 22:38:51 UTC
