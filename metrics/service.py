6208b0a943d1db2343984ad857e5323457ba7c7b
James Holloway <james.holloway94@gmail.com>
2026-06-11 05:40:42 +0000

    write bastion access log time into metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 04:28:40 UTC
+# Last sync: 2026-06-11 05:40:42 UTC
