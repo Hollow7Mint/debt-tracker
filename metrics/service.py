51e9b2a6f1292da99a7b288129aeb118d7a99c31
James Holloway <james.holloway94@gmail.com>
2026-06-11 21:06:28 +0000

    push ssh config sync marker after host refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 19:10:56 UTC
+# Last sync: 2026-06-11 21:06:28 UTC
