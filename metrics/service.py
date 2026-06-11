e3e58cffece67ef1ce52df10f1a8bfda5387ca2b
James Holloway <james.holloway94@gmail.com>
2026-06-11 02:15:35 +0000

    trace mysql table statistics sync for fleet audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 01:35:07 UTC
+# Last sync: 2026-06-11 02:15:35 UTC
