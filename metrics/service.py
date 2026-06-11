dbcc9c8a63efb02099a7c9e0711bdc5dd605cd71
James Holloway <james.holloway94@gmail.com>
2026-06-11 19:10:56 +0000

    capture db backup window completion time in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 18:26:54 UTC
+# Last sync: 2026-06-11 19:10:56 UTC
