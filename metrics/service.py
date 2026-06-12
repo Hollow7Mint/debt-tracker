c1ff0752cd4c8438f3a1b21461707c62da80c733
James Holloway <james.holloway94@gmail.com>
2026-06-12 00:38:06 +0000

    resync parameter store path time for nightly audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 23:49:03 UTC
+# Last sync: 2026-06-12 00:38:05 UTC
