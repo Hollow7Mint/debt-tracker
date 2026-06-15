6067bdbb880d22d4db77b8a6a27099b4e517f38a
James Holloway <james.holloway94@gmail.com>
2026-06-15 11:51:28 +0000

    set redis cluster sync stamp in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-15 10:21:14 UTC
+# Last sync: 2026-06-15 11:51:28 UTC
