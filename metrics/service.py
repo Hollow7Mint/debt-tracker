a29032d91e5f3687e97d53bd91ff20d455b1edbd
James Holloway <james.holloway94@gmail.com>
2026-06-16 15:54:49 +0000

    tune mongodb replication lag time for audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-16 14:43:23 UTC
+# Last sync: 2026-06-16 15:54:49 UTC
