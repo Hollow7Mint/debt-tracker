95d9fb6dfa87cc84af3663075b7b72dd5bc7737c
James Holloway <james.holloway94@gmail.com>
2026-06-17 07:25:41 +0000

    settle mysql innodb sync time after flush

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-17 06:49:05 UTC
+# Last sync: 2026-06-17 07:25:41 UTC
