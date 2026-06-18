e89fc36628636dd033f73c5638efa1db65880665
James Holloway <james.holloway94@gmail.com>
2026-06-18 04:31:22 +0000

    record kms key rotation time in config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-18 03:43:15 UTC
+# Last sync: 2026-06-18 04:31:22 UTC
