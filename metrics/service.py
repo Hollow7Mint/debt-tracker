e985520fb187005be8cf31b000bd6eb29444de73
James Holloway <james.holloway94@gmail.com>
2026-06-19 07:52:44 +0000

    retain ssh key expiry stamp for rotation audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-19 06:42:17 UTC
+# Last sync: 2026-06-19 07:52:44 UTC
