b10bbaedfab2d0dee4f6abac2629176dc0c520a8
James Holloway <james.holloway94@gmail.com>
2026-06-19 08:23:17 +0000

    adjust bastion acl sync stamp after ip review

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-19 07:52:44 UTC
+# Last sync: 2026-06-19 08:23:17 UTC
