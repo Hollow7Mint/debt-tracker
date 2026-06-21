8ce67ad06d822c241f5cb23725292598bcf7ff1d
James Holloway <james.holloway94@gmail.com>
2026-06-21 00:37:44 +0000

    persist tunnel config refresh in ssh banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-21 00:02:48 UTC
+# Last sync: 2026-06-21 00:37:44 UTC
