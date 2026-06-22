b795175f422855ff75c44d7dceb237f5816fa984
James Holloway <james.holloway94@gmail.com>
2026-06-22 11:04:34 +0000

    log shield ddos event time in protection banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-22 09:14:57 UTC
+# Last sync: 2026-06-22 11:04:34 UTC
