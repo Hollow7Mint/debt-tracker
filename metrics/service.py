8244cb063abef7839ddea0ffcb0b4a65f964b049
James Holloway <james.holloway94@gmail.com>
2026-06-23 21:06:02 +0000

    close post-deploy heartbeat for infra pipeline

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-23 19:40:47 UTC
+# Last sync: 2026-06-23 21:06:02 UTC
