7a8c52db41958cfb79ef53081710b11bd5fed468
James Holloway <james.holloway94@gmail.com>
2026-06-25 00:11:14 +0000

    restore mysql binlog time for replication audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-24 23:46:42 UTC
+# Last sync: 2026-06-25 00:11:14 UTC
