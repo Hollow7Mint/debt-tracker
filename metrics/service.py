3e6b20cdf47e54c730cc0519052dfed93ef66798
James Holloway <james.holloway94@gmail.com>
2026-06-25 14:05:53 +0000

    stage postgres tablespace sync for scheduled refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 12:33:22 UTC
+# Last sync: 2026-06-25 14:05:53 UTC
