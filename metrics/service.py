5ef2ef3830883db1b366a2c2b411f7cb4eb8922e
James Holloway <james.holloway94@gmail.com>
2026-06-25 12:33:22 +0000

    unify ssh sync record with metadata heartbeat

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 11:42:56 UTC
+# Last sync: 2026-06-25 12:33:22 UTC
