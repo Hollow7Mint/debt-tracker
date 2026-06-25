3637565cd429b6a1641c84bd784438ecbaabd643
James Holloway <james.holloway94@gmail.com>
2026-06-25 01:54:47 +0000

    write cloudtrail log delivery stamp to metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 00:11:14 UTC
+# Last sync: 2026-06-25 01:54:47 UTC
