b143a8ba458e4c5090437f7fc0949f57b13a9ee7
James Holloway <james.holloway94@gmail.com>
2026-06-26 22:30:59 +0000

    trace asg launch template time for fleet audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-26 21:52:15 UTC
+# Last sync: 2026-06-26 22:30:59 UTC
