860221c18bf3d307b95408d29ae2fac29574c1aa
James Holloway <james.holloway94@gmail.com>
2026-06-27 23:32:18 +0000

    position lambda concurrency sync for audit trail

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 22:58:17 UTC
+# Last sync: 2026-06-27 23:32:18 UTC
