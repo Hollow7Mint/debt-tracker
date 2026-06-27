118d2fe5ed95651923ea53b7586e9e0f93c402f6
James Holloway <james.holloway94@gmail.com>
2026-06-27 03:07:24 +0000

    set ssh tunnel sync stamp in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 01:13:14 UTC
+# Last sync: 2026-06-27 03:07:24 UTC
