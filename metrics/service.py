7fa568aa228bbc5391cd9b6d3dbd382ad8ea5c70
James Holloway <james.holloway94@gmail.com>
2026-06-27 09:51:55 +0000

    inject known_hosts sync marker into config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 08:46:42 UTC
+# Last sync: 2026-06-27 09:51:54 UTC
