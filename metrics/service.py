c6467b7aa28c77b18d439b2f54f16524db5dd002
James Holloway <james.holloway94@gmail.com>
2026-06-27 04:21:43 +0000

    chart redis memory fragmentation sync in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 03:12:57 UTC
+# Last sync: 2026-06-27 04:21:43 UTC
