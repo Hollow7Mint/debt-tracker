7a1344071819ee6c9e6908fbbf3568a8df3e147b
James Holloway <james.holloway94@gmail.com>
2026-06-29 16:45:51 +0000

    flag cloudwatch log retention sync in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 15:15:04 UTC
+# Last sync: 2026-06-29 16:45:51 UTC
