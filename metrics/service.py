897450731f0c07eefdf98692eea123fc2d65c3e1
James Holloway <james.holloway94@gmail.com>
2026-06-29 06:03:08 +0000

    retain redis aof sync stamp for persistence audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 05:06:13 UTC
+# Last sync: 2026-06-29 06:03:08 UTC
