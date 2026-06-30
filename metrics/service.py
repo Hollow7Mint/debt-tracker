5dc62da0e960d0718a05c72e9077d9dfb39203c5
James Holloway <james.holloway94@gmail.com>
2026-06-30 09:46:51 +0000

    preserve nat gateway sync time in infra banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-30 09:05:37 UTC
+# Last sync: 2026-06-30 09:46:51 UTC
