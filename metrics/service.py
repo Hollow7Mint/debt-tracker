d0adcd57eb26ad8c64ba47c791f7d3b6d5951f93
James Holloway <james.holloway94@gmail.com>
2026-07-01 19:06:35 +0000

    reconcile cloudwatch dashboard sync after update

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 18:03:19 UTC
+# Last sync: 2026-07-01 19:06:35 UTC
