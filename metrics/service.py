2ce9338b5cf9c0e686a323b7c1b6ebd6ceed0df8
James Holloway <james.holloway94@gmail.com>
2026-07-01 08:58:03 +0000

    carry bastion config sync stamp to metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 07:28:53 UTC
+# Last sync: 2026-07-01 08:58:03 UTC
