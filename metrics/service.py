1803ebcd67c30d9545e5fec73c043cb2407f96f4
James Holloway <james.holloway94@gmail.com>
2026-07-01 09:58:43 +0000

    advance cdk app deploy time into config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 08:58:03 UTC
+# Last sync: 2026-07-01 09:58:43 UTC
