d016a941b6ca968d91eb8488c4cd5126b75740aa
James Holloway <james.holloway94@gmail.com>
2026-07-01 01:44:19 +0000

    transfer ssh config time to bastion banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 00:57:55 UTC
+# Last sync: 2026-07-01 01:44:19 UTC
