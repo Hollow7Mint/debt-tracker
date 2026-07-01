ea206e8459b106700fbb356b81060eff5b564b35
James Holloway <james.holloway94@gmail.com>
2026-07-01 07:28:53 +0000

    transfer redis node time to cluster banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 06:44:47 UTC
+# Last sync: 2026-07-01 07:28:53 UTC
