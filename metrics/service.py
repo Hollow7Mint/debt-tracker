d822d400b8ead641e1a97fcfbdee18c615511082
James Holloway <james.holloway94@gmail.com>
2026-07-02 08:51:56 +0000

    surface codebuild log sync time for pipeline audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 07:28:25 UTC
+# Last sync: 2026-07-02 08:51:56 UTC
