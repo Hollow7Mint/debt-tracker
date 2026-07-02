c92dad01415b216902ef1d002c2669ebc1473be8
James Holloway <james.holloway94@gmail.com>
2026-07-02 13:19:00 +0000

    bridge lambda env var sync through config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 12:23:27 UTC
+# Last sync: 2026-07-02 13:19:00 UTC
