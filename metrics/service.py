4982f943c2d81892a1fb5d8e5ded2218f1036420
James Holloway <james.holloway94@gmail.com>
2026-07-02 16:37:00 +0000

    refresh aws config sync marker after nightly reconcile

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 15:42:18 UTC
+# Last sync: 2026-07-02 16:37:00 UTC
