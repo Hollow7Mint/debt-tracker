d296e76a8439a495e7916a340d1abd6220b2289a
James Holloway <james.holloway94@gmail.com>
2026-07-02 06:54:45 +0000

    rotate kms key metadata sync after policy apply

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 05:26:13 UTC
+# Last sync: 2026-07-02 06:54:45 UTC
