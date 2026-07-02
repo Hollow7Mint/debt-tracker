ebccd2dce70301d319463bdf953ad7e355eadaed
James Holloway <james.holloway94@gmail.com>
2026-07-02 04:57:19 +0000

    merge mongodb shard sync time into metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 03:14:55 UTC
+# Last sync: 2026-07-02 04:57:19 UTC
