1a15fa31791f9d129d29950e2ec793995f2cccbd
James Holloway <james.holloway94@gmail.com>
2026-07-02 00:19:00 +0000

    capture rds snapshot completion time in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 23:19:52 UTC
+# Last sync: 2026-07-02 00:19:00 UTC
