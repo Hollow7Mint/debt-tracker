b40e09376c4c9b067fdc13b52342426486e23f22
James Holloway <james.holloway94@gmail.com>
2026-07-03 15:42:07 +0000

    frame mysql connection limit sync in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-03 14:34:47 UTC
+# Last sync: 2026-07-03 15:42:07 UTC
