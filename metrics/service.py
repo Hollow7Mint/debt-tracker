f7aee72058178e43f7e61a1a062f7484032b7836
James Holloway <james.holloway94@gmail.com>
2026-07-03 23:37:42 +0000

    queue redis keyspace sync after config update

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-03 22:37:42 UTC
+# Last sync: 2026-07-03 23:37:42 UTC
