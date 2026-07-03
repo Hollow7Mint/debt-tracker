43028d2bbebdcc9d8b6f3c3a4b38647d6302615d
James Holloway <james.holloway94@gmail.com>
2026-07-03 18:25:17 +0000

    seal postgres logical replication sync in banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-03 17:40:01 UTC
+# Last sync: 2026-07-03 18:25:16 UTC
