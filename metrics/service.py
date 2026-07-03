d9ca99e12f2b5f1720ef97b8c089fdf26baba751
James Holloway <james.holloway94@gmail.com>
2026-07-03 07:04:37 +0000

    capture host key sync completion time in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-03 05:29:22 UTC
+# Last sync: 2026-07-03 07:04:37 UTC
