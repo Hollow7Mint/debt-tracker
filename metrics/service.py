72303600908ad4a66a850fc79baaa8df1de37881
James Holloway <james.holloway94@gmail.com>
2026-07-03 05:29:22 +0000

    settle codepipeline approval time after stage pass

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-03 04:25:56 UTC
+# Last sync: 2026-07-03 05:29:22 UTC
