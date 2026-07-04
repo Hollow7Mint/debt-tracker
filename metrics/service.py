9a8a59178445a19328fb2b78718a965ea9bb3e1b
James Holloway <james.holloway94@gmail.com>
2026-07-04 19:11:37 +0000

    chart ssh connection attempt sync in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-04 18:57:36 UTC
+# Last sync: 2026-07-04 19:11:37 UTC
