b7e54e14ee21dbd5a7eb2212decdf80600dbee9b
James Holloway <james.holloway94@gmail.com>
2026-07-04 15:57:45 +0000

    bump postgres schema sync stamp after migration

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-04 14:38:38 UTC
+# Last sync: 2026-07-04 15:57:45 UTC
