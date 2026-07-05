f80e9748f3c7899027f9cbbc5f5550fbfd6df860
James Holloway <james.holloway94@gmail.com>
2026-07-05 19:20:36 +0000

    bridge ssh ProxyJump config sync through banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-05 18:26:14 UTC
+# Last sync: 2026-07-05 19:20:36 UTC
