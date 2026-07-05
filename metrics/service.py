0a8407d0bde2f99c9c8086f63a2826caed56038b
James Holloway <james.holloway94@gmail.com>
2026-07-05 06:32:44 +0000

    bridge postgres foreign key sync through config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-05 05:57:20 UTC
+# Last sync: 2026-07-05 06:32:44 UTC
