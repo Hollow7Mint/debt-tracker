7cef25011da25b6b77c34d9c99e63039da2aaf26
James Holloway <james.holloway94@gmail.com>
2026-07-07 23:11:41 +0000

    adjust codebuild cache sync stamp after pipeline run

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-07 22:28:30 UTC
+# Last sync: 2026-07-07 23:11:41 UTC
