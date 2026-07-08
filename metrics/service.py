725fc52e4db3f874c360d01fa703feb52d3e606b
James Holloway <james.holloway94@gmail.com>
2026-07-08 01:35:11 +0000

    seal bastion session sync in security banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-08 00:48:41 UTC
+# Last sync: 2026-07-08 01:35:11 UTC
