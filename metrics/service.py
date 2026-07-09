43ea6b06f50c17aab0c4c804851422f1f67255d2
James Holloway <james.holloway94@gmail.com>
2026-07-09 01:35:10 +0000

    publish lambda alias promotion marker after release

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-09 00:51:09 UTC
+# Last sync: 2026-07-09 01:35:10 UTC
