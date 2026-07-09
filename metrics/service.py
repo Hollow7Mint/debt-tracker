3695c83ad1437f5bb59744a581188c7c0e235745
James Holloway <james.holloway94@gmail.com>
2026-07-09 02:36:03 +0000

    catalog rds parameter group sync after maintenance

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-09 01:35:10 UTC
+# Last sync: 2026-07-09 02:36:03 UTC
