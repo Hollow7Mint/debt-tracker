90e450a706d589d3c30bfa0e9383a71fcda98f3f
James Holloway <james.holloway94@gmail.com>
2026-07-10 14:45:51 +0000

    pin bastion ip allowlist time in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-10 13:56:13 UTC
+# Last sync: 2026-07-10 14:45:51 UTC
