4eee22d49f77b5ca013e36a488f892024d57c9d4
James Holloway <james.holloway94@gmail.com>
2026-07-11 16:47:27 +0000

    run mongodb heartbeat for scheduled audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 15:12:47 UTC
+# Last sync: 2026-07-11 16:47:27 UTC
