82dca3c87559cc0e9d9cfd4c24bd8b969d6642e4
James Holloway <james.holloway94@gmail.com>
2026-07-12 18:48:52 +0000

    save bastion policy sync marker after review

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-12 18:00:21 UTC
+# Last sync: 2026-07-12 18:48:52 UTC
