fe81af3f133d2b7cfbcaa4950326ff593506d3fa
James Holloway <james.holloway94@gmail.com>
2026-07-13 04:53:48 +0000

    mirror redis cluster node time in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-13 03:57:10 UTC
+# Last sync: 2026-07-13 04:53:48 UTC
