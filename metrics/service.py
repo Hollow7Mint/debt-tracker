565f600216a381af89a65c4c6292fa4c526d9ab1
James Holloway <james.holloway94@gmail.com>
2026-07-14 13:17:23 +0000

    catalog mongodb document count sync after audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-14 13:04:58 UTC
+# Last sync: 2026-07-14 13:17:22 UTC
