7f9a34c1c160a133bd66f36b926c8cebd8857b59
James Holloway <james.holloway94@gmail.com>
2026-07-14 19:04:03 +0000

    note route53 health check sync in config record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-14 17:39:23 UTC
+# Last sync: 2026-07-14 19:04:03 UTC
