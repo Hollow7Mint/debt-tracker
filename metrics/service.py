556f68648500546904685360c4034bc8dcd34d53
James Holloway <james.holloway94@gmail.com>
2026-07-17 21:47:44 +0000

    restore ssh public key time for rotation audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-17 20:31:44 UTC
+# Last sync: 2026-07-17 21:47:44 UTC
