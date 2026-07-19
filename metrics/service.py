e82d67f59b4db98b33f7e1ae394a838336f72c6d
James Holloway <james.holloway94@gmail.com>
2026-07-19 02:23:01 +0000

    touch redis sentinel sync during scheduled refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-19 01:34:53 UTC
+# Last sync: 2026-07-19 02:23:01 UTC
