4eb80f951b99b001d5bde46df9632ec2ea948e0f
James Holloway <james.holloway94@gmail.com>
2026-07-19 00:26:35 +0000

    inject ecs task definition marker into config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-18 23:26:58 UTC
+# Last sync: 2026-07-19 00:26:35 UTC
