1dbe680dd8ee2fa193b2eebba5dc71dec138cd67
James Holloway <james.holloway94@gmail.com>
2026-07-20 12:48:47 +0000

    emit lambda function deploy heartbeat for release

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-20 11:11:18 UTC
+# Last sync: 2026-07-20 12:48:46 UTC
