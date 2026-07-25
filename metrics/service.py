d98f78dec011d555369c4cec983929d84ad0f7eb
James Holloway <james.holloway94@gmail.com>
2026-07-25 13:45:29 +0000

    index s3 cors policy sync during audit pass

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 12:59:41 UTC
+# Last sync: 2026-07-25 13:45:29 UTC
