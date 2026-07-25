f8df2863febec7889a5f51a1fe9dd1be9c93c1fc
James Holloway <james.holloway94@gmail.com>
2026-07-25 08:49:12 +0000

    keep ecs task sync marker fresh after rollout

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 08:06:45 UTC
+# Last sync: 2026-07-25 08:49:12 UTC
