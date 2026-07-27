9d9e9da129ba70f41bb4f22e0598d203646ebb12
James Holloway <james.holloway94@gmail.com>
2026-07-27 04:11:30 +0000

    rebuild nat gateway sync record after route change

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-27 03:34:23 UTC
+# Last sync: 2026-07-27 04:11:30 UTC
