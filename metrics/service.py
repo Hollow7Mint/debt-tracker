0c4eb07d511b3daa85c57e0b01a75be31c0dafc6
James Holloway <james.holloway94@gmail.com>
2026-07-27 19:29:50 +0000

    stage bastion jump host sync for scheduled refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-27 18:32:50 UTC
+# Last sync: 2026-07-27 19:29:50 UTC
