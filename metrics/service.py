bd4704c5bbaff5f1ea653562038fe3bed53972ea
James Holloway <james.holloway94@gmail.com>
2026-07-28 08:31:06 +0000

    save postgres pool sync marker after refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-28 07:42:41 UTC
+# Last sync: 2026-07-28 08:31:05 UTC
