bec1b2cf153964d3e8ed4cbca0f456032dc28904
James Holloway <james.holloway94@gmail.com>
2026-07-30 00:39:42 +0000

    retain cloudwatch metric filter time for audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-29 23:42:22 UTC
+# Last sync: 2026-07-30 00:39:40 UTC
