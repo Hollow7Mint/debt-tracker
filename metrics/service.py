33ef45616e631c20f51a0dc7d9a779a9517cd192
James Holloway <james.holloway94@gmail.com>
2026-07-31 00:35:11 +0000

    stage rds connection pool sync for scheduled refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-30 23:23:38 UTC
+# Last sync: 2026-07-31 00:35:11 UTC
