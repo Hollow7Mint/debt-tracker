6fb677c57dc91fe92690cab94eb68e10d624e2bf
James Holloway <james.holloway94@gmail.com>
2026-08-01 14:40:52 +0000

    merge cloudformation stack apply time into record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-01 13:13:17 UTC
+# Last sync: 2026-08-01 14:40:52 UTC
