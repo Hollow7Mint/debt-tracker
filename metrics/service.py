764645d9510a8320a589d975d08c2aca6beb7259
James Holloway <james.holloway94@gmail.com>
2026-08-01 03:43:14 +0000

    audit vpc peering sync record after route update

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-01 02:34:20 UTC
+# Last sync: 2026-08-01 03:43:14 UTC
