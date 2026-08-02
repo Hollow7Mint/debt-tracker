9edf57fc542aa819489bbdde15d4774434edfb80
James Holloway <james.holloway94@gmail.com>
2026-08-02 05:09:50 +0000

    update iam policy checkpoint for scheduled audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-02 04:35:55 UTC
+# Last sync: 2026-08-02 05:09:50 UTC
