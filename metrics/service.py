3e37468272907d94e04da7dc015eae7aba8f0198
James Holloway <james.holloway94@gmail.com>
2026-08-02 10:36:15 +0000

    frame ssm document sync time in config record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-02 09:11:41 UTC
+# Last sync: 2026-08-02 10:36:14 UTC
