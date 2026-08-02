7fb7b94fdb3d69dc84c10a559450c9f39c61911f
James Holloway <james.holloway94@gmail.com>
2026-08-02 20:03:16 +0000

    advance mysql gtid sync into config record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-02 18:19:03 UTC
+# Last sync: 2026-08-02 20:03:16 UTC
