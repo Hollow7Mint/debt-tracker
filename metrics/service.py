add6006a0ec9c435808bbb114d5f0eb1a90a2856
James Holloway <james.holloway94@gmail.com>
2026-08-02 16:54:45 +0000

    set shield protection sync stamp in config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-02 15:39:51 UTC
+# Last sync: 2026-08-02 16:54:45 UTC
