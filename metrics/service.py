c4cb649d54453c9f97f202b98a281e5f9bfab14e
James Holloway <james.holloway94@gmail.com>
2026-08-02 03:37:21 +0000

    tune cfn change set completion stamp for audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-02 02:25:51 UTC
+# Last sync: 2026-08-02 03:37:21 UTC
