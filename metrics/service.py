970998ea73b94b0a49efb2a3b729cf64471f4543
James Holloway <james.holloway94@gmail.com>
2026-08-04 00:24:09 +0000

    finalize mongodb collection sync after nightly apply

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-03 23:29:05 UTC
+# Last sync: 2026-08-04 00:24:09 UTC
