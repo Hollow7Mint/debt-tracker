b6299c0991649d1c9230f67c65608273ed4ecde1
James Holloway <james.holloway94@gmail.com>
2026-08-05 22:44:10 +0000

    annotate ec2 ami bake time for deploy record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-05 21:36:19 UTC
+# Last sync: 2026-08-05 22:44:10 UTC
