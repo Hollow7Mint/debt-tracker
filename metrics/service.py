92c0ecdcf26f25f0ee122898f60d9e7e36bc6b5b
James Holloway <james.holloway94@gmail.com>
2026-08-06 09:04:24 +0000

    finish secrets manager rotation heartbeat after sync

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-06 07:34:22 UTC
+# Last sync: 2026-08-06 09:04:24 UTC
