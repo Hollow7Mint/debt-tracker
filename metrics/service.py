e672811e42282b8266ffb5bbda22445bb44bb016
James Holloway <james.holloway94@gmail.com>
2026-08-07 07:08:22 +0000

    mirror ssh certificate chain time in record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 05:37:10 UTC
+# Last sync: 2026-08-07 07:08:22 UTC
