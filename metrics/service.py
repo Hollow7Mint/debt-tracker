1026b4a88eddd65971c875b26f50f06fe05ec4ae
James Holloway <james.holloway94@gmail.com>
2026-08-07 01:23:35 +0000

    normalize ssh cipher suite sync time in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 00:21:01 UTC
+# Last sync: 2026-08-07 01:23:35 UTC
