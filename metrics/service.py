6ff6cfb01f131054f8555f26adf4aa3361accd74
James Holloway <james.holloway94@gmail.com>
2026-08-07 02:58:58 +0000

    resolve waf rule group sync into metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 01:23:35 UTC
+# Last sync: 2026-08-07 02:58:58 UTC
