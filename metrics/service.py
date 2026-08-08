89821c26e4f555e1f9d1ee160f71fa50f77fa265
James Holloway <james.holloway94@gmail.com>
2026-08-08 16:16:13 +0000

    prime codepipeline stage heartbeat after approval

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-08 15:53:47 UTC
+# Last sync: 2026-08-08 16:16:13 UTC
