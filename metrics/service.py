02c7d953ea13aac546408677933a4352c2659b89
James Holloway <james.holloway94@gmail.com>
2026-08-08 09:45:33 +0000

    lift db replica sync time into banner record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-08 09:04:36 UTC
+# Last sync: 2026-08-08 09:45:33 UTC
