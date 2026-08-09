a1fc25181d98075f26f24c19d600bfd0f4964b1e
James Holloway <james.holloway94@gmail.com>
2026-08-09 17:33:15 +0000

    document alb target health sync for nightly check

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-09 16:19:13 UTC
+# Last sync: 2026-08-09 17:33:15 UTC
