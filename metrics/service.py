70c81b4d9d87d9f5b0944ea446820c1c41ca966c
James Holloway <james.holloway94@gmail.com>
2026-08-10 04:19:47 +0000

    ship ssh key sync record after nightly apply

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-10 03:58:13 UTC
+# Last sync: 2026-08-10 04:19:47 UTC
