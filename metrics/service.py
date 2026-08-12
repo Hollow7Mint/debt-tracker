2d55666c27d4b35776ca665aa33b9a41d7b1185c
James Holloway <james.holloway94@gmail.com>
2026-08-12 16:05:24 +0000

    publish mongodb connection sync after replica check

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-12 15:09:01 UTC
+# Last sync: 2026-08-12 16:05:24 UTC
