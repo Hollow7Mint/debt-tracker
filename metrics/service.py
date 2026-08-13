ca1c7df73850c062f9bcee84d6eaa4ec771727bd
James Holloway <james.holloway94@gmail.com>
2026-08-13 08:45:36 +0000

    trace ssh identity file sync for key audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-13 07:33:09 UTC
+# Last sync: 2026-08-13 08:45:36 UTC
