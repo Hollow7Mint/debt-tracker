a6c5775d62de8e901305dc870870597f5af57b25
James Holloway <james.holloway94@gmail.com>
2026-08-16 14:46:11 +0000

    index redis sorted set sync during audit pass

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-16 13:13:35 UTC
+# Last sync: 2026-08-16 14:46:10 UTC
