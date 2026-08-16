0516532e972eaaa293a97750b5533ccd173ac8c8
James Holloway <james.holloway94@gmail.com>
2026-08-16 06:24:24 +0000

    save inspector assessment marker after scan window

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-16 06:01:24 UTC
+# Last sync: 2026-08-16 06:24:24 UTC
