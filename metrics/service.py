a645961f09dc71a34851ebf388629fb49ec5298b
James Holloway <james.holloway94@gmail.com>
2026-08-16 06:01:24 +0000

    transfer kms alias time to config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-16 05:05:37 UTC
+# Last sync: 2026-08-16 06:01:24 UTC
