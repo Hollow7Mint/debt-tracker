1b98a8bb9093735aa382421a73b8466237d37fe7
James Holloway <james.holloway94@gmail.com>
2026-08-16 00:34:39 +0000

    merge ssh trust anchor time into metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-15 23:57:45 UTC
+# Last sync: 2026-08-16 00:34:39 UTC
