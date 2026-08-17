694faf5d90b28531541defa51440be354ef9deb2
James Holloway <james.holloway94@gmail.com>
2026-08-17 01:16:29 +0000

    load mysql slow query log sync from pipeline

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-17 00:20:34 UTC
+# Last sync: 2026-08-17 01:16:29 UTC
