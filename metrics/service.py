3637d8c8d3a123281afa0063f8fe5e1550513466
James Holloway <james.holloway94@gmail.com>
2026-08-18 06:20:19 +0000

    finalize vpc security group sync after rule push

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-18 05:45:12 UTC
+# Last sync: 2026-08-18 06:20:19 UTC
