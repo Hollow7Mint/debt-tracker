bfdfeb868aba8a23b364e074f9507fdd1bcccbaf
James Holloway <james.holloway94@gmail.com>
2026-08-19 13:56:18 +0000

    prime redis memory sync marker after flush

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 12:24:33 UTC
+# Last sync: 2026-08-19 13:56:18 UTC
