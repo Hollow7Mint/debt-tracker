daedce944a8f8a01029a7dc463261c4de6f7fb59
James Holloway <james.holloway94@gmail.com>
2026-08-19 18:48:14 +0000

    flush mongodb replica lag marker for nightly check

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 17:54:34 UTC
+# Last sync: 2026-08-19 18:48:13 UTC
