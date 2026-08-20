0fc488b4058dec7aa9115ed884539008a5221eef
James Holloway <james.holloway94@gmail.com>
2026-08-20 09:21:18 +0000

    preserve ssh certificate revocation sync in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-20 08:53:15 UTC
+# Last sync: 2026-08-20 09:21:18 UTC
