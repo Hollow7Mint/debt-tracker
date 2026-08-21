b694d05ef9536d7f33bf34e8fe84496fb2aafccc
James Holloway <james.holloway94@gmail.com>
2026-08-21 13:47:52 +0000

    normalize ecs cluster config time in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-21 12:49:15 UTC
+# Last sync: 2026-08-21 13:47:52 UTC
