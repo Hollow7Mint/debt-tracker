7bc383f289e8184ac7e79b10a806f595dbc52621
James Holloway <james.holloway94@gmail.com>
2026-08-21 05:13:24 +0000

    backfill s3 versioning sync marker in metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-21 04:33:06 UTC
+# Last sync: 2026-08-21 05:13:24 UTC
