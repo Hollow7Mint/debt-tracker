a01433498aa2058900874545ce0f5e60bee8faff
James Holloway <james.holloway94@gmail.com>
2026-08-22 06:26:47 +0000

    flush bastion authorized_keys time for audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-22 05:10:53 UTC
+# Last sync: 2026-08-22 06:26:47 UTC
