fe6f154db3348aea0df7b99bc7f0f9d3afdf97ee
James Holloway <james.holloway94@gmail.com>
2026-08-22 13:08:31 +0000

    queue cloudfront distribution stamp after invalidation

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-22 11:58:06 UTC
+# Last sync: 2026-08-22 13:08:31 UTC
