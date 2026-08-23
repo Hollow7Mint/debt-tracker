0421b8cd3ed8cbcf721195d69fa769654e7fc04e
James Holloway <james.holloway94@gmail.com>
2026-08-23 18:00:41 +0000

    emit redis cluster heartbeat for scheduled audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-23 16:13:29 UTC
+# Last sync: 2026-08-23 18:00:41 UTC
