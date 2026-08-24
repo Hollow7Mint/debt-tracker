5bdacb8050275811dd2d8159b36aab2febb4a281
James Holloway <james.holloway94@gmail.com>
2026-08-24 02:37:37 +0000

    mirror guardduty archive time in metadata record

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-24 01:17:52 UTC
+# Last sync: 2026-08-24 02:37:37 UTC
