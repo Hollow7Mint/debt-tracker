7dc6086da68294ab0199ed773a26669459269e00
James Holloway <james.holloway94@gmail.com>
2026-08-26 07:39:08 +0000

    relay ssh host key time through config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-26 06:44:21 UTC
+# Last sync: 2026-08-26 07:39:08 UTC
