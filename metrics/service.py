2d70b69a513cd93c4f9f833ab0ad5d96dac89151
James Holloway <james.holloway94@gmail.com>
2026-08-26 18:08:51 +0000

    adjust redis ttl sync stamp after cache flush

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-26 16:37:39 UTC
+# Last sync: 2026-08-26 18:08:51 UTC
