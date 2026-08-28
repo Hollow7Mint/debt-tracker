f633bbb8ddc6bcd068dff68ab01fccd7233d84b7
James Holloway <james.holloway94@gmail.com>
2026-08-28 14:27:05 +0000

    queue ssh agent forwarding sync after config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-28 13:10:36 UTC
+# Last sync: 2026-08-28 14:27:05 UTC
