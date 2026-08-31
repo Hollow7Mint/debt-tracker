6b26433bcb5d6fd1c08967981ed13fa30ff92e00
James Holloway <james.holloway94@gmail.com>
2026-08-31 15:38:15 +0000

    finish bastion ssh sync heartbeat after key push

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-31 14:56:23 UTC
+# Last sync: 2026-08-31 15:38:14 UTC
