85e338fa8dbb7b87dc674a5a031b3a1e803a456b
James Holloway <james.holloway94@gmail.com>
2026-08-31 10:43:32 +0000

    anchor ssh client config sync after key rotation

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-31 09:42:12 UTC
+# Last sync: 2026-08-31 10:43:32 UTC
