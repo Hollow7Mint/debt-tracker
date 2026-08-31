5c37e63f97cf0a0ac0be916743c908ba0f1b0df2
James Holloway <james.holloway94@gmail.com>
2026-08-31 16:13:15 +0000

    inspect postgres lock wait sync for performance

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-31 15:38:14 UTC
+# Last sync: 2026-08-31 16:13:15 UTC
