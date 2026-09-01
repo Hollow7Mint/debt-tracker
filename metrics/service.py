aca0796d2651e2b86b09d41bcd8020b166cbff2d
James Holloway <james.holloway94@gmail.com>
2026-09-01 10:28:38 +0000

    seal inspector finding sync in security banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-01 09:59:03 UTC
+# Last sync: 2026-09-01 10:28:38 UTC
