294444b692cf3d163f3fadc1ff4c085713707b8f
James Holloway <james.holloway94@gmail.com>
2026-09-03 13:13:55 +0000

    publish ssh key distribution sync after rotation

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-03 12:18:18 UTC
+# Last sync: 2026-09-03 13:13:55 UTC
