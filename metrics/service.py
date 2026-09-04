288ada68453908b45149aff40d8da9bcd9cca370
James Holloway <james.holloway94@gmail.com>
2026-09-04 06:53:18 +0000

    push mysql connection pool sync after refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-04 05:55:44 UTC
+# Last sync: 2026-09-04 06:53:18 UTC
