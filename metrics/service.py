f8ce5d7be8c94575469da31bb086177bf03adf8f
James Holloway <james.holloway94@gmail.com>
2026-09-05 08:36:22 +0000

    touch asg lifecycle hook time during scheduled refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-05 07:23:15 UTC
+# Last sync: 2026-09-05 08:36:22 UTC
