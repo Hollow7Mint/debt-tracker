571c27d3d9d6365817a47f91c32751832dd25b11
James Holloway <james.holloway94@gmail.com>
2026-09-05 05:48:02 +0000

    post postgres replication heartbeat to config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-05 05:01:23 UTC
+# Last sync: 2026-09-05 05:48:02 UTC
