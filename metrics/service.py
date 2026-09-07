9759a6cafeaa761e9912cb1b55e8835b136af95b
James Holloway <james.holloway94@gmail.com>
2026-09-07 16:41:35 +0000

    store ssh revocation list sync for key audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-07 15:48:32 UTC
+# Last sync: 2026-09-07 16:41:35 UTC
