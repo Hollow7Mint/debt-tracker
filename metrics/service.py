3c0435bb9223c5ce58dd94564cc844b9878f40e5
James Holloway <james.holloway94@gmail.com>
2026-09-08 12:29:37 +0000

    relay codebuild artifact time through config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 11:17:57 UTC
+# Last sync: 2026-09-08 12:29:37 UTC
