7ebd2f197544eaf90fdffeaca17d9a9f77eb686e
James Holloway <james.holloway94@gmail.com>
2026-09-08 23:29:41 +0000

    validate bastion access time in security config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 22:37:47 UTC
+# Last sync: 2026-09-08 23:29:41 UTC
