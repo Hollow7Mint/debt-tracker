1f56afe09d4fed54b33fede8cef81599cd039b90
James Holloway <james.holloway94@gmail.com>
2026-09-08 19:08:36 +0000

    ship db schema sync record after nightly apply

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 18:01:52 UTC
+# Last sync: 2026-09-08 19:08:36 UTC
