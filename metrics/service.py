fd2bcbbde02edf53f6a589c7470c5aa7c7fe2b5a
James Holloway <james.holloway94@gmail.com>
2026-09-08 13:15:03 +0000

    relay db connection string time through config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 12:29:37 UTC
+# Last sync: 2026-09-08 13:15:03 UTC
