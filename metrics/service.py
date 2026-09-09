c2fa803ad2af75f96f1a7f4013fead8dab14920a
James Holloway <james.holloway94@gmail.com>
2026-09-09 20:03:45 +0000

    push secrets manager version stamp to metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-09 18:23:11 UTC
+# Last sync: 2026-09-09 20:03:45 UTC
