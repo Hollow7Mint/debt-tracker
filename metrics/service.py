cb5d497204ad4878a0a0634a724d35fdaedcf9a7
James Holloway <james.holloway94@gmail.com>
2026-09-10 10:20:05 +0000

    emit ssh certificate renewal heartbeat for audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-10 09:52:14 UTC
+# Last sync: 2026-09-10 10:20:05 UTC
