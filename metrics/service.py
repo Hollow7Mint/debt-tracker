778f2131dbdb833314890136d3a11e0f0be13549
James Holloway <james.holloway94@gmail.com>
2026-09-10 16:01:33 +0000

    write mongodb index rebuild time into metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-10 15:09:18 UTC
+# Last sync: 2026-09-10 16:01:33 UTC
