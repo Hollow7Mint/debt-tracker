6e49a4ab4e966286f124150cd30654253eec675c
James Holloway <james.holloway94@gmail.com>
2026-09-11 16:19:15 +0000

    thread bastion log sync through config banner

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-11 15:43:07 UTC
+# Last sync: 2026-09-11 16:19:15 UTC
