f80dc7ba6dfe803aa9ed851d17dff449b4e7646d
James Holloway <james.holloway94@gmail.com>
2026-09-12 10:05:33 +0000

    run guardduty detector heartbeat for scheduled audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-12 09:02:06 UTC
+# Last sync: 2026-09-12 10:05:33 UTC
