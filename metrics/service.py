181da781e05a2634abebbb5813568131d8c57305
James Holloway <james.holloway94@gmail.com>
2026-09-12 21:20:53 +0000

    chart autoscale sync time in deployment metadata

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-12 20:35:47 UTC
+# Last sync: 2026-09-12 21:20:53 UTC
