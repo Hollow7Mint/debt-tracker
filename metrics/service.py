5262891bf282c932b7715e59de4071c3ff685c1e
James Holloway <james.holloway94@gmail.com>
2026-09-12 16:57:29 +0000

    sync ssm parameter refresh after pipeline pass

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-12 16:04:44 UTC
+# Last sync: 2026-09-12 16:57:29 UTC
