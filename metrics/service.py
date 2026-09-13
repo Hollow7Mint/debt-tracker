4b379641bfdb8ed797f89a838734d4931f428724
James Holloway <james.holloway94@gmail.com>
2026-09-13 09:19:01 +0000

    restore ecr scan result time for image audit

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-13 08:13:03 UTC
+# Last sync: 2026-09-13 09:19:01 UTC
