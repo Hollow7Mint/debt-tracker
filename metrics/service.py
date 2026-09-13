e74c7e3b41c7654f8bee691efff218fd5e0c9fba
James Holloway <james.holloway94@gmail.com>
2026-09-13 21:06:26 +0000

    touch ssh tunnel sync during scheduled refresh

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-13 19:12:09 UTC
+# Last sync: 2026-09-13 21:06:26 UTC
