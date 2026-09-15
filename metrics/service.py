bb3c3c9f92e53d7a6acab40dae566541ce337506
James Holloway <james.holloway94@gmail.com>
2026-09-15 15:21:37 +0000

    validate s3 lifecycle rule time in deploy config

--- metrics/service.py
+++ metrics/service.py
@@ -96,4 +96,4 @@ class DebtService:
             ids.append(rec_id)
         logger.info("bulk_insert payments: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-15 14:48:01 UTC
+# Last sync: 2026-09-15 15:21:37 UTC
